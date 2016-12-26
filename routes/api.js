6671d6caf602aabef18805feccdb3c3aebe6f530
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2016-12-26 12:51:48 +0300

    routes api

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,25 @@ router.route('/posts')
   // create a new post
   .post(function (req, res) {
     // TODO create a new post in the database
-    req.send({ message: "TODO create a new post in the database" });
+    req.send({ message: 'TODO create a new post in the database' });
   })
   .get(function (req, res) {
     // TODO get all the posts in the database
-    req.send({ message: "TODO get all the posts in the database" });
+    req.send({ message: 'TODO get all the posts in the database' });
   })
+
+// api for a specific post
+router.route('/posts/:id')
+
+  // create
+  .put(function (req, res) {
+    return res.send({ message: 'TODO modify an existing post by using param ' + req.param.id });
+  })
+  .get(function (req, res) {
+    return res.send({ message: 'TODO get an existing post by using ' + req.param.id });
+  })
+  .delete(function (req, res) {
+    return res.send({ message: 'TODO delete an existing post by using param ' + req.param.id });
+  });
 
 module.exports = router;
